aadaf7fd5054d2d2e7ab750b62d10ec8374ee0b1
陈奇丰 <49940282+chenqifeng666@users.noreply.github.com>
2019-04-30 14:28:44 +0800

    Update B1004p.cpp

--- PAT (Basic Level) Practice（中文）/B1004p.cpp
+++ PAT (Basic Level) Practice（中文）/B1004p.cpp
@@ -1,3 +1,9 @@
+/*
+写的有些麻烦，主要是我想用一下结构体
+遇到困难：
+	对于结构体的复制在这种情况下是可以直接直接赋值的（结构体不存在指针），我本来想要用构造函数来赋值（可能我对结构体还没有完全理解），后来放弃，另外使用一个函数进行逐项赋值。
+	然后测试一直通不过，我也没有搞清楚具体原因，猜测是我原本使用字符数组char[10]来存储字符串,中间赋值有些问题吧。后来采用string类存储，然后将输入输出全部换成cin&cout，因为string类不能用printf直接输出。
+*/
 #include <cstring>
 #include <iostream>
 using namespace std;
@@ -24,6 +30,7 @@ int main(){
 		if(stu.score>max){
 			max=stu.score;
 			stu_max=stu_copy(stu);
+			//stu_max=stu;//不调用函数也是可以的，因为结构体中不存在指针
 		}
 		if(stu.score<min){
 			min=stu.score;
