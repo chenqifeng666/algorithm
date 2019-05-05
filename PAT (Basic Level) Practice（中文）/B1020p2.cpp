6a54b9d704013a1547d670fbdf357f47851acf3a
陈奇丰 <49940282+chenqifeng666@users.noreply.github.com>
2019-05-05 20:57:16 +0800

    Update B1020p2.cpp

--- PAT (Basic Level) Practice（中文）/B1020p2.cpp
+++ PAT (Basic Level) Practice（中文）/B1020p2.cpp
@@ -1,6 +1,7 @@
 /*
 20190505
-����������Ŀ�޶���N�Ĵ�С������ֱ�Ӷ���struct����Ҳ�ǿ��еģ����ҿ��Ը��ӷ����ʹ��sort
+发现由于题目限定了N的大小，所以直接定义struct数组也是可行的，而且可以更加方便的使用sort
+不知道为什么，答案部分正确23分
 */
 #include <iostream>
 #include <algorithm>
@@ -10,21 +11,22 @@ struct mooncake{
 	int amout;
 	int sale;
 	double price;
-}m[1001];
+}m[1010];
 
-bool cmp(struct mooncake a, struct mooncake b){return a.price>b.price;} 
+bool cmp(mooncake a, mooncake b){return a.price>b.price;} 
 int main(){
 	int N,sum,i;
 	cin >> N >> sum;
 	for(i=0;i<N;i++) cin >> m[i].amout;
 	for(i=0;i<N;i++) cin >> m[i].sale;
-	for(i=0;i<N;i++) m[i].price=m[i].sale/(double)m[i].amout;//���㵥�� 
+	for(i=0;i<N;i++) m[i].price=m[i].sale/(double)m[i].amout;//计算单价 
 	
-	sort(m,m+N,cmp);//���ݵ��۴Ӵ�С����
+	sort(m,m+N,cmp);//根据单价从大到小排序
 	
-	//̰��˼ά
-	int remain=sum,temp;
+	//贪心思维
+	int remain=sum;
 	double profit=0;
+	/*这部分有问题，可能存在供不应求的情况 
 	for(i=0;remain>0;i++){
 		temp=remain-m[i].amout;
 		if(temp>0){
@@ -34,7 +36,16 @@ int main(){
 			profit+=m[i].price*remain;
 			remain=0;
 		}
-	}
+	}*/ 
+	for(i=0;i<N;i++){
+		if(m[i].amout<=remain){
+			profit+=m[i].sale;
+			remain-=m[i].amout;
+		}else{
+			profit+=m[i].price*remain;
+			break;
+		}
+	} 
 	printf("%.2f",profit);
 	return 0;
 } 
