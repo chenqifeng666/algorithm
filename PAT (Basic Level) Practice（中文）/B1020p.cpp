35210f5d61c7400f4118b61cfa66ee590d119c96
陈奇丰 <49940282+chenqifeng666@users.noreply.github.com>
2019-05-05 17:25:06 +0800

    1020 月饼 (25 分)
    月饼是中国人在中秋佳节时吃的一种传统食品，不同地区有许多不同风味的月饼。现给定所有种类月饼的库存量、总售价、以及市场的最大需求量，请你计算可以获得的最大收益是多少。
    
    注意：销售时允许取出一部分库存。样例给出的情形是这样的：假如我们有 3 种月饼，其库存量分别为 18、15、10 万吨，总售价分别为 75、72、45 亿元。如果市场的最大需求量只有 20 万吨，那么我们最大收益策略应该是卖出全部 15 万吨第 2 种月饼、以及 5 万吨第 3 种月饼，获得 72 + 45/2 = 94.5（亿元）。
    
    输入格式：
    每个输入包含一个测试用例。每个测试用例先给出一个不超过 1000 的正整数 N 表示月饼的种类数、以及不超过 500（以万吨为单位）的正整数 D 表示市场最大需求量。随后一行给出 N 个正数表示每种月饼的库存量（以万吨为单位）；最后一行给出 N 个正数表示每种月饼的总售价（以亿元为单位）。数字间以空格分隔。
    
    输出格式：
    对每组测试用例，在一行中输出最大收益，以亿元为单位并精确到小数点后 2 位。
    
    输入样例：
    3 20
    18 15 10
    75 72 45
    输出样例：
    94.50

---
+++ PAT (Basic Level) Practice（中文）/B1020p.cpp
@@ -0,0 +1,54 @@
+/*
+20190505
+本来想建立struct mooncake结构来存储数据，但是实体数量不可知，也不可能手动建立很多实体m1,m2... 
+后来想使用vector<double> v[3];来存储输入数据以及售价，但是在排序这里就很麻烦 
+现在pat上25分题只拿到23分，错了一个算例，错误为：段错误，不知道错在哪里？？？ 
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+
+bool cmp(double a, double b){return (a > b);}
+int main(){
+	int N,sum,i,j;
+	double temp;
+	cin >> N >> sum;
+	vector<double> v[3];//v[0]存库存，v[1]存售价，v[2]存单价 
+	for(i=0;i<2*N;i++){
+		cin>>temp;
+		v[i/N].push_back(temp);  //存储输入数据 
+	}
+	for(j=0;j<N;j++){
+		temp=v[1][j] / v[0][j];
+		v[2].push_back(temp);    //算出并存储月饼单价 
+	}
+	vector<double> tempv2=v[2];
+	sort(tempv2.begin(),tempv2.end(),cmp);//从大到小排序 
+	
+	vector<double>::iterator it;
+	vector<int> index(N);
+	for(i=0;i<N;i++){
+		it=find(v[2].begin(),v[2].end(),tempv2[i]);
+//		cout << (it-v[2].begin()) << "\n";
+		index[i]=it-v[2].begin();//顺序索引，index[0]=2表示v[2]向量中的最大值在第三个数 
+	}
+	//根据贪心思想，先卖单价最高的
+	int remain=sum;
+	i=0;
+	double profit=0.0;
+	while(remain>0){ 
+		if(remain-v[0][index[i]]>=0){
+			profit+=v[1][index[i]];
+			remain-=v[0][index[i]];
+		}else{
+			profit+=v[2][index[i]]*remain;
+			remain=0;
+		}
+		i++;
+	}
+	printf("%.2f", profit);
+	return 0; 
+}
