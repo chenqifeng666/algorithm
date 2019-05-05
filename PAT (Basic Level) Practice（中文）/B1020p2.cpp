1e668a9242a946fb3f4b2329407980a5a298b4e6
陈奇丰 <49940282+chenqifeng666@users.noreply.github.com>
2019-05-05 20:13:30 +0800

    使用了struct [1000]数组，更加方便
    1020 月饼 (25 分)
    
    月饼是中国人在中秋佳节时吃的一种传统食品，不同地区有许多不同风味的月饼。现给定所有种类月饼的库存量、总售价、以及市场的最大需求量，请你计算可以获得的最大收益是多少。
    
    注意：销售时允许取出一部分库存。样例给出的情形是这样的：假如我们有 3 种月饼，其库存量分别为 18、15、10 万吨，总售价分别为 75、72、45 亿元。如果市场的最大需求量只有 20 万吨，那么我们最大收益策略应该是卖出全部 15 万吨第 2 种月饼、以及 5 万吨第 3 种月饼，获得 72 + 45/2 = 94.5（亿元）。
    
    输入格式：
    每个输入包含一个测试用例。每个测试用例先给出一个不超过 1000 的正整数 N 表示月饼的种类数、以及不超过 500（以万吨为单位）的正整数 D 表示市场最大需求量。随后一行给出 N 个正数表示每种月饼的库存量（以万吨为单位）；最后一行给出 N 个正数表示每种月饼的总售价（以亿元为单位）。数字间以空格分隔。
    
    输出格式：
    对每组测试用例，在一行中输出最大收益，以亿元为单位并精确到小数点后 2 位。
    
    输入样例：
    3 20
    18 15 10
    75 72 45
    输出样例：
    94.50

---
+++ PAT (Basic Level) Practice（中文）/B1020p2.cpp
@@ -0,0 +1,40 @@
+/*
+20190505
+发现由于题目限定了N的大小，所以直接定义struct数组也是可行的，而且可以更加方便的使用sort
+*/
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+struct mooncake{
+	int amout;
+	int sale;
+	double price;
+}m[1001];
+
+bool cmp(struct mooncake a, struct mooncake b){return a.price>b.price;} 
+int main(){
+	int N,sum,i;
+	cin >> N >> sum;
+	for(i=0;i<N;i++) cin >> m[i].amout;
+	for(i=0;i<N;i++) cin >> m[i].sale;
+	for(i=0;i<N;i++) m[i].price=m[i].sale/(double)m[i].amout;//计算单价 
+	
+	sort(m,m+N,cmp);//根据单价从大到小排序
+	
+	//贪心思维
+	int remain=sum,temp;
+	double profit=0;
+	for(i=0;remain>0;i++){
+		temp=remain-m[i].amout;
+		if(temp>0){
+			profit+=m[i].sale;
+			remain=temp;
+		}else{
+			profit+=m[i].price*remain;
+			remain=0;
+		}
+	}
+	printf("%.2f",profit);
+	return 0;
+} 
